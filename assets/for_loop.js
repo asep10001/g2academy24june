f91a2f4d4b1adc6b9472eefdd04784db5f3fabd5
asep10001 <60148057+asep10001@users.noreply.github.com>
2020-06-24 20:24:08 +0700

    Update for_loop.js

--- assets/for_loop.js
+++ assets/for_loop.js
@@ -12,14 +12,33 @@ for (jumlah2 = 1; jumlah 2 <=10; jumlah2++){
 		console.log(jumlah2 + ` adalah angka ganjil`)
 	}
 }
+var user = {
+	username: `Asep`;
+	password: `123456`;
+}
 
-function loginCheck(username, pass){
+var score = [1, 0, 1, 0, 0, 1, 0];
+
+function loginCheck(username, pass, arr){
+	var win = 0;
+	var lose = 0;
 	if(username !== 'Asep' && pass !=='123456'){
-		return console.log(`Successful Login Attempt!`)
+		console.log(`Successful Login Attempt!`)
+		for(i=0; i <= score.length; i++){
+			if(sore[i] === 1){
+				win++;
+			} else{
+				lose++;
+			}
+		}
+		if(win > lose){
+			console.log(`You are the winner`)
+		}else{
+			cpnsole.log(`You are a loser`)
 
 	} else{
-		return console.log(`username or password in incorrect`)
+		return console.log(score.length);
 	}
 }
 
-loginCheck(`Asep`,`123456`);
+loginCheck(`Asep`,`123456`, score);
