55c23383fefcfeed7004d6bcce9dd0c7d43a9bc3
asep10001 <60148057+asep10001@users.noreply.github.com>
2020-06-24 20:02:03 +0700

    menambahkan for loop

--- assets/for_loop.js
+++ assets/for_loop.js
@@ -4,3 +4,11 @@ while (jumlah < 10){
 	console.log (`i love Javascript ${jumlah}`)
 	jumlah++
 }
+
+for (jumlah2 = 1; jumlah 2 <=10; jumlah2++){
+	if (jumlah2 % 2 == 0){
+		console.log(jumlah2 + ` adalah angka genap`)
+	}else{
+		console.log(jumlah2 + ` adalah angka ganjil`)
+	}
+}
