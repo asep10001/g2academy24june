760fbe093fb10e93bbdb53de6d245ac953c23965
asep10001 <60148057+asep10001@users.noreply.github.com>
2020-06-24 20:10:03 +0700

    function 1

--- assets/for_loop.js
+++ assets/for_loop.js
@@ -12,3 +12,14 @@ for (jumlah2 = 1; jumlah 2 <=10; jumlah2++){
 		console.log(jumlah2 + ` adalah angka ganjil`)
 	}
 }
+
+function loginCheck(username, pass){
+	if(username !== 'Asep' && pass !=='123456'){
+		return console.log(`Successful Login Attempt!`)
+
+	} else{
+		return console.log(`username or password in incorrect`)
+	}
+}
+
+loginCheck(`Asep`,`123456`);
